5cd2cb1eefe0c98103ddc63056bce101b6343547
elk-bloom <152300047+elk-bloom@users.noreply.github.com>
2023-12-20 10:00:54 -0500

    deploy bash scripts and changes to test

---
+++ push_backend.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ $# -ne 1 ]; then 
+    echo "Usage: $0 <aws_account_id>"
+    exit 1
+fi
+
+aws_account_id=$1
+docker tag dwarf-backend:latest $aws_account_id.dkr.ecr.us-east-2.amazonaws.com/dwarf-backend:latest
+docker push $aws_account_id.dkr.ecr.us-east-2.amazonaws.com/dwarf-backend:latest
