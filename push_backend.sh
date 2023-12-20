ff0916b9f642390712a92cd8af8887d163d84d21
elk-bloom <152300047+elk-bloom@users.noreply.github.com>
2023-12-20 11:26:15 -0500

    push script updates

--- push_backend.sh
+++ push_backend.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
-if [ $# -ne 1 ]; then 
-    echo "Usage: $0 <aws_account_id>"
+if [ $# -ne 2 ]; then 
+    echo "Usage: $0 <aws_account_id> <aws_profile>"
     exit 1
 fi
 
-aws_account_id=$1
+
+# push new image
+aws_account_id="$1"
 docker tag dwarf-backend:latest $aws_account_id.dkr.ecr.us-east-2.amazonaws.com/dwarf-backend:latest
 docker push $aws_account_id.dkr.ecr.us-east-2.amazonaws.com/dwarf-backend:latest
+
+# force new deployment
+profile="$2"
+aws esc --profile $profile update-service --cluster dwarf-cluster --service dwarf-backend-service --force-new-deployment
--- push_frontend.sh
+++ push_frontend.sh
@@ -1,11 +1,18 @@
+
 #!/bin/bash
 set -e
 
-if [ $# -ne 1 ]; then 
-    echo "Usage: $0 <aws_account_id>"
+if [ $# -ne 2 ]; then 
+    echo "Usage: $0 <aws_account_id> <aws_profile>"
     exit 1
 fi
 
-aws_account_id=$1
+
+# push new image
+aws_account_id="$1"
 docker tag dwarf-frontend:latest $aws_account_id.dkr.ecr.us-east-2.amazonaws.com/dwarf-frontend:latest
 docker push $aws_account_id.dkr.ecr.us-east-2.amazonaws.com/dwarf-frontend:latest
+
+# force new deployment
+profile="$2"
+aws esc --profile $profile update-service --cluster dwarf-cluster --service dwarf-frontend-service --force-new-deployment
